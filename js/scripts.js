02dded68307c80fc34ffcd74265639e2d6389ef0
ahorod <prozerpina112009@gmail.com>
2017-04-07 15:48:17 -0700

    add promo code deduction, receipt styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 //business logic
 //constructor for Pizza
-function Pizza(toppings, size) {
+function Pizza(toppings, size, promo) {
   this.toppings = toppings;
   this.size = size;
+  this.promo = promo;
   this.count = 0;
   this.price = 10;
+}
+function Client (name, address, phone){
+  this.name = name;
+  this.address = address;
+  this.phone = phone;
 }
 
 Pizza.prototype.countChecked = function (){
@@ -16,9 +22,15 @@ Pizza.prototype.pizzaPrice = function() {
   if (this.size === "large") {
     this.price +=5;
   }
+  else {
+    this.price = 10
+  }
 
   if (this.count > 0) {
     this.price += this.count*2;
+  }
+  if (this.promo = "SPRING"){
+    this.price -= 3;
   }
 
   return(this.price);
@@ -42,26 +54,33 @@ $(document).ready(function() {
       }
       });
 
-    console.log(inputtedToppings);
-
     var inputtedSize= $("select.size").val();
+    var inputtedPromo= $("input#promo").val();
+    var newPizza = new Pizza(inputtedToppings, inputtedSize, inputtedPromo);
 
-    var newPizza = new Pizza(inputtedToppings, inputtedSize);
+    var inputtedName = $("input#name").val();
+    var inputtedAddress = $("input#address").val();
+    var inputtedPhone = $("input#phone").val();
+    var newClient = new Client(inputtedName, inputtedAddress, inputtedPhone);
+
     newPizza.countChecked();
-
     var resultPrice = newPizza.pizzaPrice();
 
 
       $("#show-price").show();
+      $("#client-name").text(newClient.name);
+      $("#client-address").text(newClient.address);
+      $("#client-phone").text(newClient.phone);
       $("#toppings").text(newPizza.toppings);
       $("#size").text(newPizza.size);
-      $("#price").text(resultPrice);
+      $("#price").text(resultPrice + "$");
       if (resultPrice >=15){
         $("#gift").show();
       }
       else {
         $("#gift").hide();
       }
+
 
     resetFields();
 
