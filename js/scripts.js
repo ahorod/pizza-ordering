00eacdc07a85451cf3054dfff88ab2948fd8dcbe
ahorod <prozerpina112009@gmail.com>
2017-04-07 10:23:08 -0700

    add count toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 //business logic
 //constructor for Pizza
-function Pizza(toppings, size) {
-  this.toppings = toppings;
+function Pizza(count, size) {
+  this.count = count;
   this.size = size;
   this.price = 10;
 }
-//prototype generates ticket information
-Pizza.prototype.PizzaInfo = function() {
-  return this.toppings + " " + this.size;
+
+Pizza.prototype.countChecked = function (){
+  this.count = $( "input:checked" ).length;
+  console.log(this.count);
 }
 //prototype that calculates pizza price
 Pizza.prototype.pizzaPrice = function() {
@@ -15,8 +16,8 @@ Pizza.prototype.pizzaPrice = function() {
     this.price +=5;
   }
 
-  if (this.toppings != "none") {
-    this.price +=3;
+  if (this.count > 0) {
+    this.price += this.count*2;
   }
 
   return(this.price);
@@ -34,25 +35,26 @@ $(document).ready(function() {
   $("form#pizza").submit(function(event) {
     event.preventDefault();
 
-    var inputtedToppings = [];
-    $("input[name='toppings']:checked").each(function (i) {
-      inputtedToppings[i] = $(this).val();
-    });
+    // var inputtedToppings = [];
+    // $("input[name='toppings']:checked").each(function (i) {
+    //   inputtedToppings[i] = $(this).val();
+    // });
 
     var inputtedSize= $("select.size").val();
 
-
-    var newPizza = new Pizza(inputtedToppings, inputtedSize);
+    var newPizza = new Pizza(inputtedSize);
+    var inputtedCount = newPizza.countChecked();
+    console.log(newPizza.count);
 
     var resultPrice = newPizza.pizzaPrice();
 
 
       $("#show-price").show();
 
-      $("#toppings").text(newPizza.toppings);
+      $("#toppings").html( $("input:checked" ).val());
       $("#size").text(newPizza.size);
       $("#price").text(resultPrice);
-      console.log(inputtedToppings);
+
 
     resetFields();
 
